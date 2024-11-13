99420216093a9dca723f0a6c4142ca3be9c37f77
Zin Lin Phyo <zinlinphyo.dev@gmail.com>
2024-11-13 16:32:39 +0700

    Initial Commit

---
+++ dailyvita-swiftui/dailyvita_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyvita_swiftuiApp.swift
+//  dailyvita-swiftui
+//
+//  Created by Zin Lin Phyo on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct dailyvita_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
